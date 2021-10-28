07fb1e8d48ca22d6194e67bd44f07f76799e586c
rezarinaldi <rez4rinaldi@gmail.com>
2021-10-28 20:17:04 +0700

    change laravel 7 ver

---
+++ database/migrations/2021_10_22_085914_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("category_id")->references("id")->on("categories");
+            $table->string("nm_items");
+            $table->string("description");
+            $table->integer("quantity");
+            $table->integer("price");
+            $table->integer("discount");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = array(
+            array(
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123456'),
+                'role' => 'admin',
+                'created_at' => date("Y-m-d H:i:s")
+
+            ),
+            array(
+                'name' => 'Bella',
+                'email' => 'bella@gmail.com',
+                'password' => Hash::make('bella123456'),
+                'role' => 'user',
+                'created_at' => date("Y-m-d H:i:s")
+            ),
+        );
+
+        DB::table('users')->insert($data);
+    }
+}
