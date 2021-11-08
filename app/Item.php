a2847dec3b688f52f2fa71ab68eaf285c92e29ea
b-bella99 <bella.setyowati99@gmail.com>
2021-11-09 00:53:31 +0700

    Add model Item

---
+++ app/Item.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $fillable = [
+        "category_id", "nm_items", "description", "quantity", "price", "discount"
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function pictures()
+    {
+        return $this->hasMany(Picture::class);
+    }
+}
