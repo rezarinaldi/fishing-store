dd1085329e62ee04077ef89628e51c46c3e4b53c
b-bella99 <bella.setyowati99@gmail.com>
2021-11-25 15:35:48 +0700

    Modify model item - add logic slug

--- app/Item.php
+++ app/Item.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Item extends Model
 {
@@ -23,5 +24,36 @@ class Item extends Model
     public function orders()
     {
         return $this->hasMany(Order::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($item) {
+            $item->slug = $item->createSlug($item->nm_items);
+        });
+        static::updating(function ($item) {
+            $item->slug = $item->createSlug($item->nm_items);
+        });
+    }
+
+    /** 
+     * Write code on Method
+     *
+     * @return response()
+     */
+    private function createSlug($nm_items)
+    {
+        $slug = Str::slug($nm_items, '-');
+        if (!static::where('slug', $slug)->get()->isEmpty()) {
+            $i = 1;
+            $newSlug = $slug . '-' . $i;
+            while (!static::where('slug', $newSlug)->get()->isEmpty()) {
+                $i++;
+                $newSlug = $slug . '-' . $i;
+            }
+            $slug = $newSlug;
+        }
+        return $slug;
     }
 }
