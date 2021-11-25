9ec790baddf6f3e52eb84cfd9f7ed7ad7c549b50
b-bella99 <bella.setyowati99@gmail.com>
2021-11-25 15:41:59 +0700

    Modify controller Home - add data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Item;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,6 +16,42 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        return view(
+            'pages.home',
+            [
+                'items' => Item::orderBy('created_at', 'DESC')
+                    ->take(5)
+                    ->get(),
+                'categories' => Category::get()
+            ]
+        );
+    }
+
+    public function item()
+    {
+        return view(
+            'pages.product',
+            [
+                'items' => Item::orderBy('created_at', 'DESC')
+                    ->get()
+            ]
+        );
+    }
+
+    public function detailItem($slug)
+    {
+        $item= Item::where('slug', $slug)->first();
+        return view('pages.detail_product', compact('item'),  [
+            'items' => Item::orderBy('created_at', 'DESC')
+                ->take(3)
+                ->get()
+        ]);
+    }
+    
+    public function contactus()
+    {
+        return view(
+            'pages.contact'
+        );
     }
 }
