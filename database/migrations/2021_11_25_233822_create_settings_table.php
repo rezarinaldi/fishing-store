90a934334ef81f6b5ac7a1112dff966e56b7498b
b-bella99 <bella.setyowati99@gmail.com>
2021-11-30 16:07:51 +0700

    Add table setting and coloumn TS to orders

--- database/migrations/2021_11_25_233822_create_settings_table.php
+++ database/migrations/2021_11_25_233822_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
+class CreateSettingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->string("email");
-            $table->string("phone_number");
-            $table->text("message");
+            $table->string("key");
+            $table->string("value");
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('settings');
     }
 }
