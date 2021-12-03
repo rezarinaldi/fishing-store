0ceb3af83909698b177a48074c0147910fc4fcd5
b-bella99 <bella.setyowati99@gmail.com>
2021-12-03 08:18:55 +0700

    Edit controller : order

--- app/Order.php
+++ app/Order.php
@@ -10,6 +10,8 @@ class Order extends Model
         "user_id", "item_id", "date", "quantity", "total_price", "payment_method", "payment_status", "status"
     ];
 
+    protected $guarded = [];
+
     public function item()
     {
         return $this->belongsTo(Item::class);
