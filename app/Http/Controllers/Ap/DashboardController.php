179197f77752e136dff23b351a915b54fec6ff73
b-bella99 <bella.setyowati99@gmail.com>
2021-12-03 08:21:18 +0700

    Update controller : Dashboard

--- app/Http/Controllers/Ap/DashboardController.php
+++ app/Http/Controllers/Ap/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Ap;
 
+use App\Chart;
 use App\Dashboard;
 use App\Http\Controllers\Controller;
+use App\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -15,10 +18,25 @@ class DashboardController extends Controller
 
     public function index(Dashboard $dashboard)
     {
+        $orders = Order::select(
+            DB::raw('quantity as quantity'),
+            DB::raw('total_price as total'))
+            ->oldest()->get();
+        $result[] = ['quantity','total'];
+        foreach ($orders as $key => $value) {
+            $result[++$key] = [$value->quantity, $value->total];
+            info($result);
+        }
+        // return view('productChart')->with('product_name', json_encode($result));
+        // $orderss = Order::where(DB::raw("(DATE_FORMAT(created_at,'%Y'))"),date('Y'))
+        // ->get();
+        // $orders = Order::all();
+        // return view('chart', compact('chart'));
         return view('pages\ap\dashboard', [
             'item' => $dashboard->countData('item'),
             'category' => $dashboard->countData('category'),
             'order' => $dashboard->countData('order')
-        ]);
+            // 'orders' => $orders
+        ],)->with('quantity', json_encode($result));
     }
 }
