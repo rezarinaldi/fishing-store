35460e341f0762c08bdde55bae5f057b7837f4dd
b-bella99 <bella.setyowati99@gmail.com>
2021-12-03 10:06:29 +0700

    add provider setting

---
+++ app/Providers/SettingServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use App\Setting;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class SettingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('settings', function ($app) {
+            return new Setting();
+        });
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Setting', Setting::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (!\App::runningInConsole() && count(Schema::getColumnListing('settings'))) {
+            $settings = Setting::all();
+            foreach ($settings as $key => $setting)
+            {
+                Config::set('settings.'.$setting->key, $setting->value);
+            }
+        }
+    }
+}
