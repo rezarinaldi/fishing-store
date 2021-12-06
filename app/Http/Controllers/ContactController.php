43717f84196a6b59eda88ad5955c7fe7c38edd73
b-bella99 <bella.setyowati99@gmail.com>
2021-12-06 10:34:11 +0700

    Add function create to insert message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -9,5 +10,29 @@ class ContactController extends Controller
     public function index()
     {
         return view('pages.contact');
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'message' => 'required',
+        ], [
+            'name.required' => 'Nama wajib diisi !',
+            'email.required' => 'Email wajib diisi ! | Format email salah !',
+            'phone.required' => 'Nomer telepon wajib diisi !',
+            'message.required' => 'Pesan Wajib diisi !'
+        ]);
+
+        Contact::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'message' => $request->message
+        ]);
+
+        return redirect()->route('contact')->with('success', 'Pesan sukses di kirim!');
     }
 }
