8d93c208b16281b0cbbd6cf20239672f36a3d396
b-bella99 <bella.setyowati99@gmail.com>
2021-12-10 04:32:38 +0700

    change logika for chart

--- app/Http/Controllers/Ap/DashboardController.php
+++ app/Http/Controllers/Ap/DashboardController.php
@@ -6,6 +6,7 @@ use App\Chart;
 use App\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Order;
+use App\OrderDetails;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -18,30 +19,23 @@ class DashboardController extends Controller
 
     public function index(Dashboard $dashboard)
     {
-        $orders = Order::select(
+        $orders = OrderDetails::select(
             DB::raw('year(date) as year'),
-            DB::raw('sum(quantity) as quantity'),
             DB::raw('sum(total_price) as total')
         )
             ->orderBy(DB::raw("YEAR(date)"))
             ->groupBy(DB::raw("YEAR(date)"))
             ->get();
         
-        $result[] = ['Year', 'quantity', 'total'];
+        $result[] = ['Year', 'Total Sales'];
         foreach ($orders as $key => $value) {
-            $result[++$key] = [$value->year, (int)$value->quantity, (int)$value->total];
+            $result[++$key] = [$value->year, (int)$value->total];
             info($result);
         }
-        // return view('productChart')->with('product_name', json_encode($result));
-        // $orderss = Order::where(DB::raw("(DATE_FORMAT(created_at,'%Y'))"),date('Y'))
-        // ->get();
-        // $orders = Order::all();
-        // return view('chart', compact('chart'));
         return view('pages\ap\dashboard', [
             'item' => $dashboard->countData('item'),
             'category' => $dashboard->countData('category'),
             'order' => $dashboard->countData('order')
-            // 'orders' => $orders
         ])->with('quantity', json_encode($result));
     }
 }
