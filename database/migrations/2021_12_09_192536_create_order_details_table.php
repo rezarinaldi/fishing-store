be532157145490588680911460c9b093bc39a2cd
b-bella99 <bella.setyowati99@gmail.com>
2021-12-10 04:31:39 +0700

    add migration order details

---
+++ database/migrations/2021_12_09_192536_create_order_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("order_id")->references('id')->on('orders');
+            $table->foreignId("item_id")->references('id')->on('items');
+            $table->datetime("date");
+            $table->integer("quantity");
+            $table->integer("total_price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
