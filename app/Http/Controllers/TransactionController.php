7db9c34fba2b9845b496c76ad7a45be7ce14fbe5
b-bella99 <bella.setyowati99@gmail.com>
2021-12-20 16:58:00 +0700

    Update controller transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Item;
+use App\Order;
+use App\OrderDetails;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     public function index()
     {
-        return view('pages.transaction');
+        $sellTransactions = Order::all();
+        $detail = OrderDetails::all();
+        return view('pages.transaction', compact('sellTransactions', 'detail'));
     }
 
-    public function details()
+    public function details(Order $transaction)
     {
-        return view('pages.transaction-detail');
+        // $transaction = Order::all();
+        $item = Item::all();
+        return view('pages.transaction-detail', compact('transaction', 'item'));
     }
 }
