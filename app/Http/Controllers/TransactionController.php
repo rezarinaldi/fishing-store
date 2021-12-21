d361cdae94da4364d80093360dbfe9a28218398b
b-bella99 <bella.setyowati99@gmail.com>
2021-12-21 09:44:57 +0700

    Add function for update transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -19,7 +19,34 @@ class TransactionController extends Controller
     public function details(Order $transaction)
     {
         // $transaction = Order::all();
-        $item = Item::all();
-        return view('pages.transaction-detail', compact('transaction', 'item'));
+        // $item = Item::all();
+        return view('pages.transaction-detail', compact('transaction'));
+    }
+
+    public function update(Request $request, Order $transaction)
+    {
+        $request->validate([
+            'transfers_slip.required' => 'Bukti pembayaran harus di upload'
+        ]);
+
+        $image = $request->transfer_slip;
+
+        if ($request->has('image') == '') {
+            if ($image == '') {
+                //
+            } else {
+                $nmFile = $image->getClientOriginalName();
+                $value = $image->move(public_path('/images/items/'), $nmFile);
+
+                $transaction->update([
+                    'user_id' => $request->user_id,
+                    'shipping_method' => $request->shipping_method,
+                    'transfers_slip' => $nmFile,
+                    'status' => 'process'
+                ]);
+            }
+        }
+
+        return redirect()->route('transaction-detail', $transaction)->with('success', 'Sukses menambahkan bukti pembayaran !');
     }
 }
