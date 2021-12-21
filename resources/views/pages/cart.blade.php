3e2dff60b36715641c85c24160b3a60a91ebad9a
rezarinaldi <rez4rinaldi@gmail.com>
2021-12-21 13:34:01 +0700

    update cart

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -54,10 +54,13 @@ Cart | {{ config('settings.name') }}
                                 @php $total += $details['price'] * $details['quantity'] @endphp
                                 <tr>
                                     <td style="width: 25%;">
-                                        <input type="number" id="item_id" name="item_id" value="{{ $details['id'] }}" hidden>
+                                        <input type="number" id="item_id" name="item_id" value="{{ $details['id'] }}"
+                                            hidden>
                                         <input type="date" id="date" name="date" value="{{date('Y-m-d')}}" hidden>
-                                        <input type="number" id="user_id" name="user_id" value="{{Auth::user()->id}}" hidden>
-                                        <img src="{{ asset('images/items/'.$details['image']) }}" alt="" class="cart-image" />
+                                        <input type="number" id="user_id" name="user_id" value="{{Auth::user()->id}}"
+                                            hidden>
+                                        <img src="{{ asset('images/items/'.$details['image']) }}" alt=""
+                                            class="cart-image" />
                                     </td>
                                     <td style="width: 25%;">
                                         <div class="product-title">
@@ -65,16 +68,16 @@ Cart | {{ config('settings.name') }}
                                         </div>
                                     </td>
                                     <td style="width: 25%;">
-                                        <div class="col">
-                                            <div class="input-group input-spinner">
-                                                <div class="input-group-prepend">
-                                                    <button class="btn btn-light" type="button" id="button-plus"> <i class="fa fa-minus"></i> </button>
-                                                </div>
-                                                <input type="text" name="quantity" id="quantity" class="form-control" value="{{ $details['quantity'] }}">
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-light" type="button" id="button-minus"> <i class="fa fa-plus"></i> </button>
-                                                </div>
-                                            </div>
+                                        <div class="product-title">
+                                            {{-- <div class="input-group-prepend">
+                                                <button class="btn btn-light" type="button" id="button-plus"> <i
+                                                        class="fa fa-minus"></i> </button>
+                                            </div> --}}
+                                            {{ $details['quantity'] }}
+                                            {{-- <div class="input-group-append">
+                                                <button class="btn btn-light" type="button" id="button-minus"> <i
+                                                        class="fa fa-plus"></i> </button>
+                                            </div> --}}
                                         </div>
                                     </td>
                                     <td style="width: 25%;">
@@ -82,9 +85,9 @@ Cart | {{ config('settings.name') }}
                                         </div>
                                     </td>
                                     <td style="width: 20%;">
-                                        <a href="#" class="btn btn-remove-cart">
+                                        {{-- <a href="#" class="btn btn-remove-cart">
                                             Remove
-                                        </a>
+                                        </a> --}}
                                     </td>
                                 </tr>
                                 @empty
@@ -111,37 +114,43 @@ Cart | {{ config('settings.name') }}
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="address">Address</label>
-                            <input type="text" class="form-control" id="address" name="address" value="{{ Auth::user()->address }}" readonly />
+                            <input type="text" class="form-control" id="address" name="address"
+                                value="{{ Auth::user()->address }}" readonly />
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="regency">Regency</label>
-                            <input type="text" class="form-control" id="regency" name="regency" value="{{ Auth::user()->regency }}" readonly />
+                            <input type="text" class="form-control" id="regency" name="regency"
+                                value="{{ Auth::user()->regency }}" readonly />
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="province">Province</label>
-                            <input type="text" class="form-control" id="province" name="province" value="{{ Auth::user()->province }}" readonly />
+                            <input type="text" class="form-control" id="province" name="province"
+                                value="{{ Auth::user()->province }}" readonly />
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="postal_code">Postal Code</label>
-                            <input type="text" class="form-control" id="postal_code" name="postal_code" value="{{ Auth::user()->postal_code }}" readonly />
+                            <input type="text" class="form-control" id="postal_code" name="postal_code"
+                                value="{{ Auth::user()->postal_code }}" readonly />
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="country">Country</label>
-                            <input type="text" class="form-control" id="country" name="country" value="{{ Auth::user()->country }}" readonly />
+                            <input type="text" class="form-control" id="country" name="country"
+                                value="{{ Auth::user()->country }}" readonly />
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="phone_number">Mobile</label>
-                            <input type="text" class="form-control" id="phone_number" name="phone_number" value="{{ Auth::user()->phone_number }}" readonly />
+                            <input type="text" class="form-control" id="phone_number" name="phone_number"
+                                value="{{ Auth::user()->phone_number }}" readonly />
                         </div>
                     </div>
                     <div class="col-md-4">
